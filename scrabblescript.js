8c71c66fafb5f1fc519ac3ade04660941b18be26
Valtteri <valtterih@icloud.com>
2021-03-09 21:54:02 +0200

    replaced onclick w/ eventlisteners, minor fixes

--- scrabblescript.js
+++ scrabblescript.js
@@ -1,22 +1,191 @@
-window.onload = function (){
+window.onload = function() {
+    const finLetter = [
+        {
+            letter: 'A',
+            amount: '10',
+            value: '1'
+        },
+        {
+            letter: 'B',
+            amount: '1',
+            value: '8'
+        },
+        {
+            letter: 'C',
+            amount: '1',
+            value: '10'
+        },
+        {
+            letter: 'D',
+            amount: '1',
+            value: '7'
+        },
+        {
+            letter: 'E',
+            amount: '8',
+            value: '1'
+        },
+        {
+            letter: 'F',
+            amount: '1',
+            value: '8'
+        },
+        {
+            letter: 'G',
+            amount: '1',
+            value: '8'
+        },
+        {
+            letter: 'H',
+            amount: '2',
+            value: '4'
+        },
+        {
+            letter: 'I',
+            amount: '10',
+            value: '1'
+        },
+        {
+            letter: 'J',
+            amount: '2',
+            value: '4'
+        },
+        {
+            letter: 'K',
+            amount: '5',
+            value: '2'
+        },
+        {
+            letter: 'L',
+            amount: '5',
+            value: '2'
+        },
+        {
+            letter: 'M',
+            amount: '3',
+            value: '3'
+        },
+        {
+            letter: 'N',
+            amount: '9',
+            value: '1'
+        },
+        {
+            letter: 'O',
+            amount: '5',
+            value: '2'
+        },
+        {
+            letter: 'P',
+            amount: '2',
+            value: '4'
+        },
+        {
+            letter: 'R',
+            amount: '2',
+            value: '4'
+        },
+        {
+            letter: 'S',
+            amount: '7',
+            value: '1'
+        },
+        {
+            letter: 'T',
+            amount: '9',
+            value: '1'
+        },
+        {
+            letter: 'U',
+            amount: '4',
+            value: '3'
+        },
+        {
+            letter: 'V',
+            amount: '2',
+            value: '4'
+        },
+        {
+            letter: 'Y',
+            amount: '2',
+            value: '4'
+        },
+        {
+            letter: 'Ä',
+            amount: '5',
+            value: '2'
+        },
+        {
+            letter: 'Ö',
+            amount: '1',
+            value: '7'
+        },
+        {
+            letter: "",
+            amount: '2',
+            value: ""
+        }
+    ];
 
-    const allletters = ['A','B','C','D','E','F','G','H', 'I','J','K','L','M','N','O','P','R','S','T','U','V','Y','Ä','Ö',' '];
-    const allids = ['let-a','let-b','let-c','let-d','let-e','let-f','let-g','let-h','let-i','let-j','let-k','let-l','let-m',
-                    'let-n','let-o','let-p','let-r','let-s','let-t','let-u','let-v','let-y','let-ä','let-ö','let-null'];
-    const allamounts = ['10','1','1','1','8','1','1','2','10','2','5','5','3','9','5','2','2','7','9','4','2','2','5','1','2'];
-    const allvalues = ['1','8','10','7','1','8','8','4', '1','4','2','2','3','1','2','4','4','1','1','3','4','4','2','7',' '];
-
-    for (var i = 0; i < allletters.length; i++) {
+    for (var i = 0; i < finLetter.length; i++) {
+        let makeSmall = finLetter[i].letter.toLowerCase();
         document.getElementById("tiles-all").innerHTML += 
         '<div class="full-let">' +
-            '<div class="letter" onclick="clickdown(\''+allids[i]+'\')">' +
-                '<div class="alphabet">'+allletters[i]+'</div>' +
-                '<div class="val">'+allvalues[i]+'</div>' +
+            '<div class="letter">' +
+                `<div class="alphabet">${finLetter[i].letter}</div>` +
+                `<div class="val">${finLetter[i].value}</div>` +
             '</div>' +
-            '<div class="undo" onclick="clickup(\''+allids[i]+'\')">x' + 
-                '<span class="amount" id='+allids[i]+'>'+allamounts[i]+'</span>' +
-                '<div class="origamount">'+allamounts[i]+'</div>' +
+            '<div class="undo">x' + 
+                `<span class="amount" id="let-${makeSmall}">${finLetter[i].amount}</span>` +
+                `<div class="origamount">${finLetter[i].amount}</div>` +
             '</div>' +
         '</div>';
     };
+
+    let clickedLetter = document.querySelectorAll(".letter");
+    for (i = 0; i<clickedLetter.length; i++){
+        clickedLetter[i].addEventListener("click", function() {
+            let deduct = this.nextElementSibling.firstElementChild;
+            let firstValue = parseInt(deduct.innerHTML);
+            let bagvalue = document.getElementById("tiles-left").innerHTML;
+            let z = parseInt(bagvalue);
+            if (firstValue == 0) {
+                console.log("kaik on jo mänt");
+            } else {
+                deduct.innerHTML = firstValue - 1;
+                if (bagvalue < 1){
+                    console.log("pussista loppu jo palikat");
+                } else {
+                    document.getElementById("tiles-left").innerHTML = z - 1;
+                }
+            }
+            let hehe = this.nextElementSibling.firstElementChild;
+            let lastValue = parseInt(hehe.innerHTML);
+            if (lastValue == 0){
+                console.log("onko sama kuin alempi"+this.innerHTML)
+                this.classList.add("is-empty");
+            }
+        });
+    };
+    let clickedUndo = document.querySelectorAll(".undo");
+    for (i = 0; i < clickedUndo.length; i++){
+        clickedUndo[i].addEventListener("click", function(){
+            let add = this.firstElementChild;
+            let orig = parseInt(add.nextElementSibling.innerHTML);
+            let x = parseInt(add.innerHTML);
+            let bagvalue = document.getElementById("tiles-left").innerHTML;
+            let z = parseInt(bagvalue);
+            if (x == orig){
+                console.log("ei voi olla laattoja enempää ku alussa");
+            } else {
+                add.innerHTML = x + 1;
+                document.getElementById("tiles-left").innerHTML = z + 1;
+            }
+            let checkup = this.firstElementChild.innerHTML;
+            let opac = this.parentNode.firstElementChild;
+            if (parseInt(checkup) !== 0) {
+                opac.classList.remove("is-empty");
+            }
+        })
+    }
 };
